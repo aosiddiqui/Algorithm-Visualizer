160ac707efff01c0e75efb3bcf1be1b63bf60328
abusid <abuosamasiddiqui1999@gmail.com>
2020-06-13 09:10:05 +0530

    fixed minor bugs

--- algoviz2.py
+++ algoviz2.py
@@ -44,14 +44,14 @@ class App:
     def __init__(self):
         self.data = []
 
-    def drawData(self, colorArray, data = []):
+    def drawData(self, data, colorArray):
         canvas.delete("all")
         c_height = 380
         c_width = 600
-        x_width = c_width/(len(self.data)+1)
+        x_width = c_width/(len(data)+1)
         offset = 30
         spacing = 10
-        normalizedData = [ i / (max(self.data)) for i in self.data]
+        normalizedData = [ i / (max(data)) for i in data]
         for i, height in enumerate(normalizedData):
             #top left
             x0 = i * x_width + offset + spacing
@@ -62,8 +62,8 @@ class App:
             y1 = c_height
 
             canvas.create_rectangle(x0,y0,x1,y1, fill=colorArray[i])
-            canvas.create_text(x0+2, y0, anchor=SW, text = str(self.data[i]), fill = 'white')
-            
+            canvas.create_text(x0+2, y0, anchor=SW, text = str(data[i]), fill = 'white')
+        self.data = data
         root.update_idletasks()
 
 
@@ -76,21 +76,21 @@ class App:
         self.data = []
         for _ in range(size):
             self.data.append(random.randrange(minVal, maxVal+1))
-        self.drawData(['yellow' for x in range(len(self.data))])
+        self.drawData(self.data, ['yellow' for x in range(len(self.data))])
 
     def StartAlgorithm(self):
         if not self.data: return
 
         if algMenu.get() == 'Quick Sort':
             quickSort(self.data, 0, len(self.data)-1, self.drawData, speedScale.get())
-        
+
         elif algMenu.get() == 'Bubble Sort':
             BSort.bubbleSort(self.data, self.drawData, speedScale.get())
 
         elif algMenu.get() == 'Merge Sort':
             mergeSort(self.data, self.drawData, speedScale.get())
-        
-        drawData(['green' for x in range(len(self.data))])
+
+        self.drawData(self.data, ['green' for x in range(len(self.data))])
 
 my_app = App()
 
