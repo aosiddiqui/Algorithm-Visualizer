d216d9c8b032ecabd34ca7fdcbbd15f02c26307c
abusid <abuosamasiddiqui1999@gmail.com>
2020-06-13 09:10:53 +0530

    fixed minor bugs in bubble_sort.py

--- bubble_sort.py
+++ bubble_sort.py
@@ -4,11 +4,14 @@ class BSort:
 
     def bubbleSort(data, drawData, timeTick):
         for i in range(len(data)-1):
-            for j in range(len(data)- i-1):
+
+            for j in range(len(data) - i-1):
                 drawData(data, ['blue' if x == j or x == j+1 else 'yellow' for x in range(len(data))])
                 time.sleep(timeTick)
 
                 if data[j] > data[j+1]:
                     data[j], data[j+1] = data[j+1], data[j]
+                    drawData(data, ['blue' if x == j or x == j+1 else 'yellow' for x in range(len(data))])
                     time.sleep(timeTick)
-        drawData(['blue' for x in range(len(data))], data)
+
+            time.sleep(timeTick)
